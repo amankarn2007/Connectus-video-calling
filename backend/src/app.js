5644cbf9021de706e311d324ab379130c8e83159
Aman Karn <amankarn2007@gmail.com>
2025-11-22 11:00:55 +0530

    updated app.js in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config();
+}
+
 const express = require("express");
 const http = require("http");
 const { createServer } = require("node:http");
@@ -6,24 +10,20 @@ const cors = require("cors");
 const {connectToSocket} = require("./controllers/socketManager.js");
 const userRoute = require("./routes/userRoute.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/Zoom_clone"; //Local DB URL
-async function main(){
-    await mongoose.connect(MONGO_URL,{
-        auth: {
-            username: "amanAdmin",
-            password: "Backend@987"
-        },
-        authSource: "admin"
-    });
-}
 
-main()
-    .then( () =>{
-        console.log("connected to DB");
-    })
-    .catch( (err) =>{
-        console.log(err);
-    })
+//const LocalMongo_Url = "mongodb://127.0.0.1:27017/Zoom_clone"; //Local DB URL
+//async function main(){
+//    await mongoose.connect(LocalMongo_Url,{
+//        auth: {
+//            username: "amanAdmin",
+//            password: "Backend@987"
+//        },
+//        authSource: "admin"
+//    });
+//}
+//main()
+//    .then(() => console.log("connected to DB"))
+//    .catch((err) => console.log(err))
 
 
 const app = express();
@@ -40,8 +40,17 @@ app.use(express.urlencoded({limit: "40kb", extended:true}));
 app.use("/api/v1/users", userRoute);
 
 const start = async() => {
-    server.listen(app.get("port"), () => {
-        console.log("server is listning on port 8080");
-    })
+    const MONGO_URL = process.env.ATLASDB_URL;
+
+    try {
+        const connectionDb = await mongoose.connect(MONGO_URL);
+        console.log(`✅ MONGO Connectet To: ${connectionDb.connection.host}`);
+        
+        server.listen(app.get("port"), () => {
+            console.log(`Server is listening on port ${app.get("port")}`);
+        });
+    }catch (error) {
+        console.error("MongoDB connection failed:", error);
+    }
 }
 start();
